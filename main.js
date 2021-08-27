7bc6fa67c13fad2067bbe51a2d8ec38dd6b8ec1e
claudio <claudiochagasff@gmail.com>
2021-08-27 15:02:45 -0300

    aprendendo js

---
+++ main.js
@@ -0,0 +1,71 @@
+function clicou() {
+    alert("Obrigado por clicar");
+}
+
+function redirecionar() {
+    window.open("https://digitalinnovation.one/");
+}
+
+function Trocar() {
+    alert("Trocar texto");
+}
+
+function funcao_change(elemento) {
+    alert(elemento.value);
+}
+
+
+
+
+/*
+function soma(n1, n2) {
+    return n1 + n2;
+}
+alert(soma(5, 10));
+*/
+
+
+
+
+
+
+/*
+var d = new Date();
+alert(d.getHours())
+*/
+
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count)
+}
+*/
+
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    count = count + 1;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?")
+
+var idade = 18;
+if (idade >= 18) {
+    alert("Maior de idade")
+}else{
+    alert("menor de idade")
+}
+*/
+
+/*
+var nome = "Claudio Chagas";
+var idade = 19;
+
+
+//alert(nome + " tem" + idade + " anos");
+
+alert(idade * idade);
+*/
